4ad39079d0f43738e3f3a71e7667df6830ee8a52
Samuel Åkesson <sermuns@lysator.liu.se>
2026-06-18 11:07:55 +0200

    working PoC for fetching gitignore in web version

--- is-it-slop-web/src/main.rs
+++ is-it-slop-web/src/main.rs
@@ -1,4 +1,8 @@
 use dioxus::prelude::*;
+use is_it_slop::{
+    github::{GitHubProject, fetch_gitignore},
+    reqwest,
+};
 
 pub const PKG_NAME: &str = "is-it-slop";
 
@@ -8,9 +12,26 @@ fn main() {
 
 #[component]
 fn App() -> Element {
+    let mut contents = use_signal(String::new);
+
+    let fetch = move |_| async move {
+        let project = GitHubProject {
+            repo: "ampy".into(),
+            owner: "HSScodes".into(),
+            url: None,
+        };
+        let client = reqwest::Client::new();
+        let gitignore = fetch_gitignore(&project, "HEAD", &client).await.unwrap();
+        contents.set(gitignore);
+    };
+
     rsx! {
         Stylesheet { href: asset!("/style.css") }
 
-        h1 {{PKG_NAME}}
+        h1 { {PKG_NAME} }
+
+        button { onclick: fetch }
+
+        code { "{contents}" }
     }
 }
--- is-it-slop/src/lib.rs
+++ is-it-slop/src/lib.rs
@@ -1,2 +1,4 @@
 pub mod crate_metadata;
 pub mod github;
+
+pub use reqwest;
